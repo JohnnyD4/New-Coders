0f2570b6ebc7b1d3c26a26fd0fbd2892b1d834c0
John Davis <johnsdavis95@gmail.com>
2017-07-07 09:52:29 -0400

    I need more time

--- server.js
+++ server.js
@@ -27,16 +27,64 @@ connection.connect(function(err) {
 })
 
 app.get("/", function(req, res) {
-	connection.query("SELECT `sample-submission`.project_id, `test`.name, `test`.country, `test`.goal, `sample-submission`.final_status, `test`.desc, test.launched_at, test.deadline FROM  `test` INNER JOIN `sample-submission` ON  `sample-submission`.project_id = `test`.`project_id` WHERE `sample-submission`.final_status LIMIT 10", function(err, data) {
+
+	// connection.query("SELECT ")
+	connection.query("SELECT `sample-submission`.project_id, `test`.name, `test`.country, `test`.goal, `sample-submission`.final_status, `test`.desc, test.launched_at, test.deadline FROM test INNER JOIN `sample-submission` ON  `sample-submission`.project_id = `test`.`project_id` WHERE `sample-submission`.final_status LIMIT 5", function(err, data) {
 
 		if (err) throw err;
 
-		res.render('index', {test: data});
+		connection.query('SELECT avg(goal) AS avg FROM train WHERE final_status = 1', function(err, tableData){
+
+			var finalData = {
+				test: data,
+				nextTable: tableData,
+				title: 'Home Page'
+			}
+
+			// orm.showtable('asdf', function(data) {
+
+			// })
+			console.log(tableData);		
+
+			res.render('index', finalData);
+		})
+
+		
 	})
-	
+		
 
 })
 
+
+
+app.post("/edit/:id", function(req, res) {
+	// console.log(req.body.name);
+	// console.log(req.body.goal);
+	// console.log(req.params.id);
+	connection.query("UPDATE `test` SET `test`.name = ?, `test`.goal = ?, `test`.desc = ? WHERE `sample-submission`.project_id = ?", [req.body.name, req.body.goal, req.body.desc, req.params.id], function(err, data) {
+
+		if (err) throw err;
+
+		res.redirect("/");
+	})
+
+})
+
+app.get("/edit/:id", function(req, res) {
+
+	connection.query("SELECT `sample-submission`.project_id, `test`.name, `test`.country, `test`.goal, `sample-submission`.final_status, `test`.desc, test.launched_at, test.deadline FROM test INNER JOIN `sample-submission` ON  `sample-submission`.project_id = `test`.`project_id` WHERE `sample-submission`.final_status AND `sample-submission`.`project_id` = ? LIMIT 5", [req.params.id], function(err, data) {
+
+		if (err) throw err;
+
+		var finalData= {
+			test: data,
+			title: "Edit"
+		}
+
+		res.render('edit', finalData);
+	})
+	// res.render("edit");
+})
 
 app.listen(PORT, function(err, data) {
 	console.log(PORT);
